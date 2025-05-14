f74e6ddf76d6d4c08ce259053fba78b11a047198
hooman619zz <hooman69khanloo@gmail.com>
2025-05-14 09:09:52 +0330

    init

---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "newadmin",
+    Password = "s0m3p4ssw0rd"
+};
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "MyQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var consumerEvent = new AsyncEventingBasicConsumer(channel);
+
+consumerEvent.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+await channel.BasicConsumeAsync("MyQueue", true, consumerEvent);
+Console.WriteLine(" [x] Received message");
+Console.ReadLine();
---
+++ RabbitProducer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "newadmin",
+    Password = "s0m3p4ssw0rd"
+};
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "MyQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+
+
+string message = "Hello World! Producer";
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: "", routingKey: "MyQueue", body: body);
+Console.WriteLine("Message Has Sent !");
+Console.ReadLine();

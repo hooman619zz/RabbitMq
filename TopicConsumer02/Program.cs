87fd16f7e8ce7d5bfab56af438f4ba5ab360da0a
hooman619zz <hooman69khanloo@gmail.com>
2025-05-18 11:51:16 +0330

    rabbit mq with Topi Exchange

---
+++ TopicConsumer02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Consumer 02 :");
+Console.WriteLine("-------------------------------------");
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "newadmin",
+    Password = "s0m3p4ssw0rd"
+};
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+
+string exchangeName = "TopicEx";
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Topic);
+
+string routingKey = "RK01.*.error";
+string queueName = (await channel.QueueDeclareAsync()).QueueName;
+await channel.QueueBindAsync(queue: queueName, exchange: exchangeName, routingKey: routingKey);
+
+var consumerEvent = new AsyncEventingBasicConsumer(channel);
+
+consumerEvent.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+await channel.BasicConsumeAsync(queueName, true, consumerEvent);
+Console.WriteLine(" [x] Received message");
+Console.ReadLine();
---
+++ TopicProducer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World!");
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "newadmin",
+    Password = "s0m3p4ssw0rd"
+};
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+string exchangeName = "TopicEx";
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Topic);
+
+bool isRepeat = true;
+while (isRepeat)
+{
+    Console.WriteLine("Enter Command:");
+    var command = Console.ReadLine();
+    switch (command)
+    {
+        case "p":
+            Console.WriteLine("Enter Routing Key:");
+            string routingKey = Console.ReadLine();
+            
+            Console.WriteLine("Enter Message:");
+            string message = Console.ReadLine();
+            
+            var body = Encoding.UTF8.GetBytes(message!);
+
+            await channel.BasicPublishAsync(exchange: exchangeName, routingKey: routingKey, body: body);
+            Console.WriteLine("-------------------------------------");
+            break;
+        case "e":
+            isRepeat = false;
+            break;
+    }
+}
+
+
+Console.WriteLine("Message Has Sent !");
+Console.ReadLine();

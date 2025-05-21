b1a2b1cf08f3fdde086b8c2b46c0c6f889def1c5
hooman619zz <hooman69khanloo@gmail.com>
2025-05-21 12:10:00 +0330

    request reply

---
+++ RequestReplayConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Consumer :");
+Console.WriteLine("-------------------------------------");
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "newadmin",
+    Password = "s0m3p4ssw0rd"
+};
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+var replyQueue = await channel.QueueDeclareAsync(queue: "", durable: true, exclusive: true);
+await channel.QueueDeclareAsync("request-queue", exclusive: false);
+
+
+var consumerEvent = new AsyncEventingBasicConsumer(channel);
+
+consumerEvent.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Reply message : {message}");
+};
+
+await channel.BasicConsumeAsync(replyQueue.QueueName, true, consumerEvent);
+
+var properties = new BasicProperties();
+properties.ReplyTo = replyQueue.QueueName;
+properties.CorrelationId = Guid.NewGuid().ToString();
+
+string message = "send request consumer ";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(addr: new PublicationAddress(null, null, "request-queue"), basicProperties: properties, body);
+
+Console.WriteLine("Consumer Finished");
+Console.ReadLine();
---
+++ RequestReplayProducer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "newadmin",
+    Password = "s0m3p4ssw0rd"
+};
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("request-queue", exclusive: false);
+
+
+var consumerEvent = new AsyncEventingBasicConsumer(channel);
+
+consumerEvent.ReceivedAsync += async (model, ea) =>
+{
+    Console.WriteLine($"CorrelationId : {ea.BasicProperties.CorrelationId}");
+    Console.WriteLine($"ReplyTo : {ea.BasicProperties.ReplyTo}");
+
+
+    var message2 = $"this message is response for : {ea.BasicProperties.CorrelationId}";
+    var body2 = Encoding.UTF8.GetBytes(message2);
+    await channel.BasicPublishAsync("", routingKey: ea.BasicProperties.ReplyTo, body2);
+};
+await channel.BasicConsumeAsync("request-queue", true, consumerEvent);
+Console.ReadLine();
